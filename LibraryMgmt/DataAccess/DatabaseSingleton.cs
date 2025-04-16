b0e850065a3daff920e3811781e2421a730c696c
cthruglaz <105048951+DeadByBattery@users.noreply.github.com>
2025-04-17 02:33:16 +0800

    Update singleton file

--- LibraryMgmt/DataAccess/DatabaseSingleton.cs
+++ LibraryMgmt/DataAccess/DatabaseSingleton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,29 @@ namespace LibraryMgmt.DataAccess
 {
     internal class DatabaseSingleton
     {
+        private static DatabaseSingleton? _instance;
+        private readonly SQLiteConnection _connection;
+        
+        public DatabaseSingleton()
+        {
+            string databasePath = Path.Combine(Application.StartupPath, @"..\..\..\Database", "test2.db");
+            string connectionString = $"Data Source={Path.GetFullPath(databasePath)};Version=3;";
+            _connection = new SQLiteConnection(connectionString);
+            _connection.Open();
+        }
+
+        public static DatabaseSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new DatabaseSingleton();
+                }
+                return _instance;
+            }
+        }
+
+        public SQLiteConnection Connection => _connection;
     }
 }
